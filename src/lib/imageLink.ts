142c2b2bacb6a4e008f18609d198cc0f600d283d
Moritz Roessler <moritz.roessler@digitas.com>
2026-01-31 16:48:16 +0100

    fix: make url fall back

--- src/lib/imageLink.ts
+++ src/lib/imageLink.ts
@@ -1,6 +1,6 @@
 export const imageLink = (url: string) => {
   const relativeOrAbsoluteUrl = url;
-  if (!url || url.includes("s3.eu")) return "/images/wallpaper/19.webp";
+  if (!url || url?.includes("s3.eu")) return "/images/wallpaper/19.webp";
   return relativeOrAbsoluteUrl?.startsWith("http")
     ? relativeOrAbsoluteUrl
     : `${process.env.STRAPI_URL}${relativeOrAbsoluteUrl}`;
