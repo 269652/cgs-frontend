56b834cb0e5d1db804cd85f5ad6b7a66b3d88abb
Moritz Roessler <moritz.roessler@digitas.com>
2026-01-31 15:14:42 +0100

    fix: add tmp image whitelist

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'cgs-strapi-production.up.railway.app'],
+    domains: ['localhost', 'cgs-strapi-production.up.railway.app', 'cgs.reactserver.dev'],
   },
 };
 
